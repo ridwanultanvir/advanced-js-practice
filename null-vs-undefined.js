ee70e313293fc02bd1b9e685a7cf95367f2dcfe3
preetom-saha-arko <1305001.psa@ugrad.cse.buet.ac.bd>
2020-05-29 13:16:44 +0600

    null vs undefined added

---
+++ null-vs-undefined.js
@@ -0,0 +1,30 @@
+let a;
+console.log(a);
+
+function add(a,b)
+{
+    console.log(a,b);
+    console.log(a+b);
+    return;
+}
+
+a = add(4,2);
+console.log(a);
+
+a = add(4)
+console.log(a);
+
+a = {name:"arko", phone:123};
+console.log(a.gf);
+
+a = [2,5];
+console.log(a[4]);
+
+a = undefined;
+console.log(a);
+
+a = 5;
+console.log(a);
+
+a = null;
+console.log(a);
